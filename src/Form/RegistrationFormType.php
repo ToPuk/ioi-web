b8e5ef5264aba7c4940a4156f88fc094e53d0216
Orgil Batsaikhan <orgil.num@gmail.com>
2022-05-18 14:45:35 +0800

    update

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,18 +2,12 @@
 
 namespace App\Form;
 
-use App\Entity\EMBaiguullaga;
 use App\Entity\User;
-use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
@@ -21,7 +15,6 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
-    private $embRegNum;
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -30,9 +23,6 @@ class RegistrationFormType extends AbstractType
             ->add('embRegNum', TextType::class, [
                 'mapped' => false,
             ])
-            ->add('emb', EntityType::class, [
-                'class' => EMBaiguullaga::class,
-                'choice_label' => 'companyName' ])
             ->add('lastName')
             ->add('firstName')
             ->add('phoneNumber')
@@ -64,45 +54,6 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
         ;
-        $formModifier = function (FormInterface $form, string $emb = null) {
-        $this->embRegNum = null === $emb ? null : $emb;
-
-        $form->add('emb', EntityType::class, [
-                'class' => EMBaiguullaga::class,
-                'choice_label' => 'companyName',
-                'label' => 'Байгууллага:',
-                'placeholder' => '',
-                'required' => false,
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('e')
-                        ->andWhere('e.regNum = :embRegNum')
-                        ->setParameter('embRegNum', $this->embRegNum);
-                }
-            ]);
-        };
-
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($formModifier) {
-
-                $data = $event->getData();
-
-                $formModifier($event->getForm(), $data->getEmb());
-            }
-        );
-
-        $builder->get('embRegNum')->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $event) use ($formModifier) {
-                // It's important here to fetch $event->getForm()->getData(), as
-                // $event->getData() will get you the client data (that is, the ID)
-                $emb = $event->getForm()->getData();
-                // since we've added the listener to the child, we'll have to pass on
-                // the parent to the callback functions!
-                $formModifier($event->getForm()->getParent(), $emb);
-            }
-        );
-
     }
 
     public function configureOptions(OptionsResolver $resolver): void
